7ec528fc89b80729e3faa001eb6c61c019d660f0
Aleksandr <rozhkov_a@outlook.com>
2024-09-22 18:08:39 +0300

    fix multiply

--- src/main/java/FastMatrixCell.java
+++ src/main/java/FastMatrixCell.java
@@ -1,3 +1,5 @@
+
+import java.util.HashMap;
 import java.util.HashSet;
 
 import org.ejml.data.DMatrixSparseCSC;
@@ -20,25 +22,26 @@ public class FastMatrixCell extends AbstractMatrix {
 
         if (isCell(other.matrix)) {
             tmp = new DMatrixSparseCSC(matrix.numCols, matrix.numRows);
-            
+
             asistant.getMatrix("cell", n).matrix.zero();
 
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(m, other.matrix, tmp);
+                multHelper(m, other.matrix, tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix, null, null);
             }
         } else {
-            tmp = new DMatrixSparseCSC(Math.min(matrix.numCols, matrix.numRows), Math.max(matrix.numRows, matrix.numCols));
-            
             asistant.getMatrix("horizon", n).matrix.zero();
 
+            tmp = asistant.getMatrix("horizon", n).matrix.copy();
+
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(m, BlockHelper.revolutionToTheHorizon(other.matrix), tmp);
+
+                multHelper(m, BlockHelper.revolutionToTheHorizon(other.matrix), tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix, null, null);
             }
 
             if (!production.endsWith("_i")) {
-                    asistant.putMatrix(n, BlockHelper.reverseVectorBlockMatrix(asistant.getMatrix(n)));
+                asistant.putMatrix(n, BlockHelper.reverseVectorBlockMatrix(asistant.getMatrix(n)));
             }
         }
     }
@@ -52,18 +55,18 @@ public class FastMatrixCell extends AbstractMatrix {
             asistant.getMatrix("cell", n).matrix.zero();
 
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(other, m, tmp);
+                multHelper(other, m, tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix,
                         null, null);
             }
         } else {
-            tmp = new DMatrixSparseCSC(Math.max(matrix.numCols, matrix.numRows), Math.min(matrix.numRows, matrix.numCols));
-
             asistant.getMatrix("vertical", n).matrix.zero();
+
+            tmp = asistant.getMatrix("vertical", n).matrix.copy();
 
             for (DMatrixSparseCSC m : matrices) {
 
-                CommonOps_DSCC.mult(BlockHelper.revolutionToTheVertical(other), m, tmp);
+                multHelper(BlockHelper.revolutionToTheVertical(other), m, tmp);
 
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix,
                         null, null);
@@ -78,21 +81,29 @@ public class FastMatrixCell extends AbstractMatrix {
     @Override
     public void add(AbstractMatrix other, AsistantMatrix asistant, int n) {
         boolean f = true;
+        boolean check = false;
+
         while (f) {
             f = false;
             for (DMatrixSparseCSC m : matrices) {
-                if (m.nz_length * b >= other.nz_length() || other.nz_length() * b >= m.nz_length) { 
+                if (m.nz_length * b >= other.nz_length() || other.nz_length() * b >= m.nz_length) {
                     CommonOps_DSCC.add(1.0, m, 1.0, other.matrix, asistant.getMatrix("cell", n).matrix, null, null);
-                    matrices.remove(m);
+                    if (m.nz_length != asistant.getMatrix(n).matrix.nz_length) {
 
-                    other.matrix = asistant.getMatrix(n).matrix.copy();
+                        matrices.remove(m);
 
-                    f = true;
+                        other.matrix = asistant.getMatrix(n).matrix.copy();
+
+                        f = true;
+                        check = true;
+                    }
                     break;
                 }
             }
         }
-        matrices.add(other.matrix.copy());
+        if (check) {
+            matrices.add(other.matrix.copy());
+        }
     }
 
     @Override
@@ -100,7 +111,7 @@ public class FastMatrixCell extends AbstractMatrix {
         if (!isCell(other.matrix)) {
             throw new IllegalArgumentException("The matrix is not cell.");
         }
-        
+
         for (DMatrixSparseCSC m : matrices) {
 
             for (int col = 0; col < m.numCols; col++) {
@@ -128,5 +139,73 @@ public class FastMatrixCell extends AbstractMatrix {
 
     private boolean isCell(DMatrixSparseCSC m) {
         return (m.numCols == this.matrix.numCols && m.numRows == this.matrix.numRows);
+    }
+
+    private void multHelper(DMatrixSparseCSC m, DMatrixSparseCSC other, DMatrixSparseCSC result) {
+
+        if (m.nz_length >= other.nz_length) {
+            multColumnByColumn(m, other, result);
+        } else {
+            multRowByRow(m, other, result);
+        }
+    }
+
+    private static void multRowByRow(DMatrixSparseCSC A, DMatrixSparseCSC B, DMatrixSparseCSC result) {
+        HashMap<Integer, Integer> freq = transform(A.nz_rows);
+        int counter;
+
+        for (int tmp : freq.keySet()) {
+            if (tmp < result.numRows) {
+                counter = 0;
+                for (int k = 0; k < A.numCols; k++) {
+                    if (k < B.numRows) {
+                        if (A.get(tmp, k) > 0) {
+                            counter++;
+                            for (int j = 0; j < B.numCols; j++) {
+                                if (B.get(k, j) > 0) {
+                                    result.set(tmp, j, 1);
+                                }
+                            }
+
+                            if (counter == freq.get(tmp)) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static HashMap<Integer, Integer> transform(int[] arr) {
+        HashMap<Integer, Integer> freqHashMap = new HashMap<>();
+
+        for (int num : arr) {
+            freqHashMap.put(num, freqHashMap.getOrDefault(num, 0) + 1);
+        }
+
+        return freqHashMap;
+    }
+
+    private static void multColumnByColumn(DMatrixSparseCSC A, DMatrixSparseCSC B, DMatrixSparseCSC result) {
+        for (int j = 0; j < B.numRows; j++) {
+            int colStartB = B.col_idx[j];
+            int colEndB = B.col_idx[j + 1];
+
+            if (colStartB != colEndB) {
+                for (int bi = colStartB; bi < colEndB; bi++) {
+                    int rowB = B.nz_rows[bi];
+
+                    int colStartA = A.col_idx[rowB];
+                    int colEndA = A.col_idx[rowB + 1];
+
+                    for (int ai = colStartA; ai < colEndA; ai++) {
+                        int rowA = A.nz_rows[ai];
+
+                        result.set(rowA, j, 1);
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/FastMatrixVector.java
+++ src/main/java/FastMatrixVector.java
@@ -1,3 +1,5 @@
+
+import java.util.HashMap;
 import java.util.HashSet;
 
 import org.ejml.data.DMatrixSparseCSC;
@@ -17,17 +19,17 @@ public class FastMatrixVector extends AbstractMatrix {
     @Override
     public void multiply(AbstractMatrix other, AsistantMatrix asistant, int n, String production) {
         DMatrixSparseCSC tmp = new DMatrixSparseCSC(Math.max(matrix.numCols, matrix.numRows), Math.min(matrix.numCols, matrix.numRows));
-        
+
         asistant.getMatrix("vertical", n).matrix.zero();
 
         if (isVector(other.matrix)) {
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(BlockHelper.toDiagMatrix(m), BlockHelper.revolutionToTheVertical(other.matrix), tmp);
+                multHelper(BlockHelper.toDiagMatrix(m), BlockHelper.revolutionToTheVertical(other.matrix), tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix("vertical", n).copy().matrix, asistant.getMatrix("vertical", n).matrix, null, null);
             }
         } else {
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(BlockHelper.revolutionToTheVertical(m), other.matrix, tmp);
+                multHelper(BlockHelper.revolutionToTheVertical(m), other.matrix, tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix("vertical", n).copy().matrix, asistant.getMatrix("vertical", n).matrix, null, null);
             }
         }
@@ -38,19 +40,21 @@ public class FastMatrixVector extends AbstractMatrix {
     }
 
     public void multiplyOther(DMatrixSparseCSC other, AsistantMatrix asistant, int n, String production) {
-        DMatrixSparseCSC tmp = new DMatrixSparseCSC(Math.max(matrix.numCols, matrix.numRows), Math.min(matrix.numCols, matrix.numRows));
+        DMatrixSparseCSC tmp;
 
         if (isVector(other)) {
+            tmp = new DMatrixSparseCSC(Math.max(matrix.numCols, matrix.numRows), Math.min(matrix.numCols, matrix.numRows));
             asistant.getMatrix("vertical", n).matrix.zero();
             for (DMatrixSparseCSC m : matrices) {
-                
-                CommonOps_DSCC.mult(BlockHelper.toDiagMatrix(other), BlockHelper.revolutionToTheVertical(m), tmp);
+
+                multHelper(BlockHelper.toDiagMatrix(other), BlockHelper.revolutionToTheVertical(m), tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix, null, null);
             }
         } else {
+            tmp = new DMatrixSparseCSC(Math.min(matrix.numCols, matrix.numRows), Math.max(matrix.numCols, matrix.numRows));
             asistant.getMatrix("horizon", n).matrix.zero();
             for (DMatrixSparseCSC m : matrices) {
-                CommonOps_DSCC.mult(other, BlockHelper.revolutionToTheHorizon(m), tmp);
+                multHelper(other, BlockHelper.revolutionToTheHorizon(m), tmp);
                 CommonOps_DSCC.add(1.0, tmp, 1.0, asistant.getMatrix(n).copy().matrix, asistant.getMatrix(n).matrix, null, null);
             }
         }
@@ -63,18 +67,23 @@ public class FastMatrixVector extends AbstractMatrix {
     @Override
     public void add(AbstractMatrix other, AsistantMatrix asistant, int n) {
         boolean f = true;
+        boolean check = false;
 
         if (matrix.numRows > matrix.numCols) {
             while (f) {
                 f = false;
                 for (DMatrixSparseCSC m : matrices) {
-                    if (m.nz_length * b >= other.nz_length() || other.nz_length() * b >= m.nz_length) { 
+                    if (m.nz_length * b >= other.nz_length() || other.nz_length() * b >= m.nz_length) {
                         CommonOps_DSCC.add(1.0, m, 1.0, BlockHelper.revolutionToTheVertical(other.matrix), asistant.getMatrix("vertical", n).matrix, null, null);
-                        matrices.remove(m);
+                        if (m.nz_length != asistant.getMatrix(n).matrix.nz_length) {
 
-                        other.matrix = asistant.getMatrix(n).matrix.copy();
+                            matrices.remove(m);
 
-                        f = true;
+                            other.matrix = asistant.getMatrix(n).matrix.copy();
+
+                            f = true;
+                            check = true;
+                        }
                         break;
                     }
                 }
@@ -82,24 +91,30 @@ public class FastMatrixVector extends AbstractMatrix {
         } else {
             while (f) {
                 f = false;
-                
+
                 for (DMatrixSparseCSC m : matrices) {
-                    
+
                     if (m.nz_length * b >= other.nz_length() || other.nz_length() * b >= m.nz_length) {
                         CommonOps_DSCC.add(1.0, m, 1.0, BlockHelper.revolutionToTheHorizon(other.matrix), asistant.getMatrix("horizon", n).matrix, null, null);
-                        matrices.remove(m);
+                        if (m.nz_length != asistant.getMatrix(n).matrix.nz_length) {
 
-                        other.matrix = asistant.getMatrix(n).matrix.copy();
+                            matrices.remove(m);
 
-                        f = true;
+                            other.matrix = asistant.getMatrix(n).matrix.copy();
+
+                            f = true;
+                            check = true;
+                        }
                         break;
                     }
                 }
 
             }
         }
-        
-        matrices.add(other.matrix.copy());
+
+        if (check) {
+            matrices.add(other.matrix.copy());
+        }
     }
 
     @Override
@@ -119,7 +134,7 @@ public class FastMatrixVector extends AbstractMatrix {
                             int row = m.nz_rows[idx];
 
                             if (m.get(row, col) > 0) {
-                                other.matrix.remove(row  % matrix.numCols, col + (matrix.numRows / (matrix.numRows / matrix.numCols)) * (row / matrix.numCols));
+                                other.matrix.remove(row % matrix.numCols, col + (matrix.numRows / (matrix.numRows / matrix.numCols)) * (row / matrix.numCols));
                             }
                         }
                     }
@@ -135,13 +150,13 @@ public class FastMatrixVector extends AbstractMatrix {
                             int row = m.nz_rows[idx];
 
                             if (m.get(row, col) > 0) {
-                                other.matrix.remove(row + (matrix.numCols / (matrix.numCols / matrix.numRows)) * (col / matrix.numRows), col  % matrix.numRows);
+                                other.matrix.remove(row + (matrix.numCols / (matrix.numCols / matrix.numRows)) * (col / matrix.numRows), col % matrix.numRows);
                             }
                         }
                     }
                 }
             }
-            
+
         } else {
 
             for (DMatrixSparseCSC m : matrices) {
@@ -177,5 +192,74 @@ public class FastMatrixVector extends AbstractMatrix {
     private boolean isVector(DMatrixSparseCSC m) {
         return ((m.numCols == this.matrix.numCols && m.numRows == this.matrix.numRows)
                 || (m.numCols == this.matrix.numRows && m.numRows == this.matrix.numCols));
+    }
+
+    private void multHelper(DMatrixSparseCSC m, DMatrixSparseCSC other, DMatrixSparseCSC result) {
+        if (m.nz_length >= other.nz_length) {
+            multColumnByColumn(m, other, result);
+        } else {
+            multRowByRow(m, other, result);
+        }
+    }
+
+    private static void multRowByRow(DMatrixSparseCSC A, DMatrixSparseCSC B, DMatrixSparseCSC result) {
+        HashMap<Integer, Integer> freq = transform(A.nz_rows);
+        int counter;
+
+        for (int tmp : freq.keySet()) {
+            if (tmp < result.numRows) {
+                counter = 0;
+                for (int k = 0; k < A.numCols; k++) {
+                    if (k < B.numRows) {
+                        if (A.get(tmp, k) > 0) {
+                            counter++;
+                            for (int j = 0; j < B.numCols; j++) {
+                                if (B.get(k, j) > 0) {
+                                    result.set(tmp, j, 1);
+                                }
+                            }
+
+                            if (counter == freq.get(tmp)) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static HashMap<Integer, Integer> transform(int[] arr) {
+        HashMap<Integer, Integer> freqHashMap = new HashMap<>();
+
+        for (int num : arr) {
+            freqHashMap.put(num, freqHashMap.getOrDefault(num, 0) + 1);
+        }
+
+        return freqHashMap;
+    }
+
+    private static DMatrixSparseCSC multColumnByColumn(DMatrixSparseCSC A, DMatrixSparseCSC B, DMatrixSparseCSC result) {
+        for (int j = 0; j < B.numCols; j++) {
+            int colStartB = B.col_idx[j];
+            int colEndB = B.col_idx[j + 1];
+
+            if (colStartB != colEndB) {
+                for (int bi = colStartB; bi < colEndB; bi++) {
+                    int rowB = B.nz_rows[bi];
+
+                    int colStartA = A.col_idx[rowB];
+                    int colEndA = A.col_idx[rowB + 1];
+
+                    for (int ai = colStartA; ai < colEndA; ai++) {
+                        int rowA = A.nz_rows[ai];
+
+                        result.set(rowA, j, 1);
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
